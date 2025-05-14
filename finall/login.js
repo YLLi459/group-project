3a791d2192662208730df02c15c9311352e7d4bd
YLLi459 <yllaliuy1@gmail.com>
2025-05-14 20:16:44 +0200

    ASDS

--- finall/login.js
+++ finall/login.js
@@ -1,13 +1,13 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+// document.getElementById("loginForm").addEventListener("submit", function(event) {
+//     event.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
 
-    if (username === "" || password === "") {
-        alert("Please fill in all fields");
-    } else {
+//     if (username === "" || password === "") {
+//         alert("Please fill in all fields");
+//     } else {
         
-        window.location.href = "dashboard.html";  
-    }
-});
+//         window.location.href = "dashboard.html";  
+//     }
+// });
